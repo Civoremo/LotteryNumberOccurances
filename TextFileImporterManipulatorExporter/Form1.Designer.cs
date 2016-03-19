90526f7884b5a171e8210b5d7322008ef067c67e
Civoremo <omerovic_n@hotmail.com>
2016-03-18 23:27:20 -0400

    Analyze PowerBall number Draws
    Using official draw data, show the number of times a number has been
    drawn.

---
+++ TextFileImporterManipulatorExporter/Form1.Designer.cs
@@ -0,0 +1,109 @@
+﻿namespace TextFileImporterManipulatorExporter
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listTextFileDisplay = new System.Windows.Forms.ListBox();
+            this.btnLoad = new System.Windows.Forms.Button();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.listTextCollection = new System.Windows.Forms.ListBox();
+            this.listBoxCounted = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // listTextFileDisplay
+            // 
+            this.listTextFileDisplay.FormattingEnabled = true;
+            this.listTextFileDisplay.Location = new System.Drawing.Point(12, 55);
+            this.listTextFileDisplay.Name = "listTextFileDisplay";
+            this.listTextFileDisplay.Size = new System.Drawing.Size(156, 173);
+            this.listTextFileDisplay.TabIndex = 0;
+            // 
+            // btnLoad
+            // 
+            this.btnLoad.Location = new System.Drawing.Point(12, 12);
+            this.btnLoad.Name = "btnLoad";
+            this.btnLoad.Size = new System.Drawing.Size(75, 23);
+            this.btnLoad.TabIndex = 1;
+            this.btnLoad.Text = "Load";
+            this.btnLoad.UseVisualStyleBackColor = true;
+            this.btnLoad.Click += new System.EventHandler(this.btnLoad_Click);
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(93, 12);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(75, 23);
+            this.btnSave.TabIndex = 2;
+            this.btnSave.Text = "Save";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // listTextCollection
+            // 
+            this.listTextCollection.FormattingEnabled = true;
+            this.listTextCollection.Location = new System.Drawing.Point(174, 55);
+            this.listTextCollection.Name = "listTextCollection";
+            this.listTextCollection.Size = new System.Drawing.Size(148, 173);
+            this.listTextCollection.TabIndex = 3;
+            // 
+            // listBoxCounted
+            // 
+            this.listBoxCounted.FormattingEnabled = true;
+            this.listBoxCounted.Location = new System.Drawing.Point(328, 55);
+            this.listBoxCounted.Name = "listBoxCounted";
+            this.listBoxCounted.Size = new System.Drawing.Size(150, 173);
+            this.listBoxCounted.TabIndex = 4;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(492, 250);
+            this.Controls.Add(this.listBoxCounted);
+            this.Controls.Add(this.listTextCollection);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.btnLoad);
+            this.Controls.Add(this.listTextFileDisplay);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "MainForm";
+            this.Text = "Text File Import/Export App";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listTextFileDisplay;
+        private System.Windows.Forms.Button btnLoad;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.ListBox listTextCollection;
+        private System.Windows.Forms.ListBox listBoxCounted;
+    }
+}
+
---
+++ TextFileImporterManipulatorExporter/Form1.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TextFileImporterManipulatorExporter
+{
+    public partial class MainForm : Form
+    {
+
+        int itemCount;
+
+
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+
+        private void btnLoad_Click(object sender, EventArgs e)
+        {            
+            EditTextFromFile();
+            OrganizeNumbers();
+            CountNumberOccurences();
+        } // end btnLoadClick
+
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+
+            System.IO.StreamWriter SaveFiles = new System.IO.StreamWriter(@"C:\Users\Netko\Documents\Visual Studio 2015\Projects\TextFileImporterManipulatorExporter\lottoNumbers.txt");
+            System.IO.StreamWriter SaveNumberFile = new System.IO.StreamWriter(@"C:\Users\Netko\Documents\Visual Studio 2015\Projects\TextFileImporterManipulatorExporter\frequentPBNumbers.txt");
+
+            foreach (var item in listTextFileDisplay.Items)
+            {
+                SaveFiles.WriteLine(item);
+            }
+            SaveFiles.Close();
+
+            foreach(var item in listBoxCounted.Items)
+            {
+                SaveNumberFile.WriteLine(item);
+            }
+            SaveNumberFile.Close();
+
+            MessageBox.Show("Text Saved");
+        } // end btnSaveClick
+
+
+        public void EditTextFromFile()
+        {
+            string[] lines = System.IO.File.ReadAllLines(@"C:\Users\Netko\Documents\Visual Studio 2015\Projects\TextFileImporterManipulatorExporter\pbNumbers.txt");
+            
+            foreach (string line in lines)
+            { 
+                string tempString = line.Remove(0, 11);
+                tempString = tempString.Remove(21);
+                
+                listTextFileDisplay.Items.Add(tempString);
+            }
+
+            listTextFileDisplay.Items.RemoveAt(0);
+            itemCount = listTextFileDisplay.Items.Count;
+        } // end EditTextFromFile
+
+
+        public void OrganizeNumbers()
+        {
+            for(int x = 0; x < itemCount; x++)
+            {
+                string[] numbers;
+
+                string tempString = listTextFileDisplay.Items[x].ToString();
+
+                numbers = tempString.Split(' ');
+
+                foreach (string num in numbers)
+                {
+                    if(num != "")
+                    listTextCollection.Items.Add(num);
+                }
+            }
+           // listTextCollection.Items.Add(listTextCollection.Items.Count);
+        } //end CountNumbers
+
+
+        public void CountNumberOccurences()
+        {
+            for (int count = 0; count < 69; count++)
+            {
+                listBoxCounted.Items.Add("0");
+            }
+
+            for (int g = 0; g < listTextCollection.Items.Count; g++)
+            {
+                int tempNum;
+                int tempText;
+                int countNum;
+
+                tempNum = Convert.ToInt16(listTextCollection.Items[g].ToString());
+
+                tempText = Convert.ToUInt16(listBoxCounted.Items[tempNum - 1].ToString());
+                countNum = tempText;
+                countNum = countNum + 1;
+                listBoxCounted.Items[tempNum - 1] = Convert.ToString(countNum);
+                
+            } // end for
+
+            for (int t = 0; t < listBoxCounted.Items.Count; t++)
+            {
+                string tempTxt = listBoxCounted.Items[t].ToString();
+
+                listBoxCounted.Items[t] = ("" + (t + 1) + ":\t" + tempTxt);
+            } // end for
+        } // end CountNumberOccurance
+    } // end Main
+}
